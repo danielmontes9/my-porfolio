2ede8889897e986ec93e60b4858f95988d240970
danielmontes9 <gdanielmontesh9@gmail.com>
2023-02-18 20:29:15 -0600

    router with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch:'full', redirectTo: 'home' },
+  { path: 'home', loadChildren: () => import('./core/home/home.module').then(m => m.HomeModule) },
+  { path: 'projects', loadChildren:() => import('./core/projects/projects.module').then(m => m.ProjectsModule) },
+  { path: '**', pathMatch:'full', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
