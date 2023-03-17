2bb777c5eb9d2741142a754e6bb97e683f83f587
danielmontes9 <gdanielmontesh9@gmail.com>
2023-03-16 20:15:08 -0600

    projects description

--- src/app/core/projects/projects.component.ts
+++ src/app/core/projects/projects.component.ts
@@ -7,42 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  projectsN: any[] = [1,2,3,4,5]
   projects: any[] = [
+    {
+      image: 'https://i.postimg.cc/vZwvgGXY/mini-aliendefense.png',
+      link: 'https://danielmontes-aliendefense.netlify.app/',
+      title: 'Alien Defense',
+      description: 'Mi primer videojuego que he diseñado y programado con Phaser.',
+      year: '2018'
+    },
     {
       image: 'https://i.postimg.cc/2y7YFcQV/preview-card.png',
       link: 'https://danielmontes9.github.io/StatsPreviewCardComponent/',
       title: 'Card Component',
-      description: 'xdxdxd',
-      year: '2018'
-    },
-    {
-      image: 'https://i.postimg.cc/vZwvgGXY/mini-aliendefense.png',
-      // https://aliendefense.netlify.app/
-      link: 'https://danielmontes9.github.io/alien-defense/',
-      title: 'Alien Defense',
-      description: 'xdxdxd',
+      description: '"Card Component" con diseño responsivo, reto de FrontendMentor.',
       year: '2019'
     },
     {
       image: 'https://i.postimg.cc/ZKYPP9Zq/preview-apprendiendo.png',
       link: 'https://danydeveloper9.github.io/apprendiendo/index.html',
       title: 'APPRENDIENDO',
-      description: 'xdxdxd',
+      description: 'App educativa con ejercicios, para mejorar tu gramática.',
       year: '2020'
     },
     {
       image: 'https://i.postimg.cc/1z5W7WDL/preview-cipemx.png',
       link: 'https://cipe.mx/',
       title: 'CIPE.MX',
-      description: 'xdxdxd',
+      description: 'Landing Page responsiva para CIPE, empresa enfocada en consultoría.',
       year: '2021'
     },
     {
       image: 'https://i.postimg.cc/zfr1MkV5/mini-mapas.png',
       link: 'https://maxal-cloud.com.mx/MapaZac/index.html',
       title: 'Dashboard Dinámico',
-      description: 'xdxdxd',
+      description: 'Panel de información con datos estadísticos y gráficos de Zacatecas.',
       year: '2022'
     },
   ]
